564279d2826258b7333acf873fb94e245ddf55a8
Zielin0 <lamaxdmilo@gmail.com>
2021-10-12 19:08:18 +0200

    feat(command): this.getMemberFromMention()

--- src/utils/structures/BaseCommand.js
+++ src/utils/structures/BaseCommand.js
@@ -5,4 +5,17 @@ module.exports = class BaseCommand {
     this.aliases = aliases;
     this.ownerOnly = ownerOnly || false;
   }
+
+  /**
+   * Gets member from mention
+   * @param {Message} message
+   * @param {string} mention
+   */
+  getMemberFromMention(message, mention) {
+    if (!mention) return;
+    const matches = mention.match(/^<@!?(\d+)>$/);
+    if (!matches) return;
+    const id = matches[1];
+    return message.guild.members.cache.get(id);
+  }
 };
